6896ec585e43c78333e43a2caa387152f9ea53e9
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2024-02-15 13:18:23 +0800

    feat: Implemented datatables via server side

---
+++ webroot/js/dataTables.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("#dataTable").DataTable({
+        ajax: {
+            url: "/product/getData",
+            dataSrc: "data",
+        },
+        processing: true,
+        serverSide: true,
+        columns: [
+            { data: "product_id" },
+            { data: "name" },
+            {
+                data: "price",
+                render: function (data, type, row) {
+                    return new Intl.NumberFormat("en-ph", {
+                        style: "currency",
+                        currency: "PHP",
+                    }).format(data);
+                },
+            },
+            { data: "stock_quantity" },
+            { data: "category" },
+            {
+                data: "",
+                render: function (data, type, row) {
+                    return `<a class="btn btn-primary"  href="${editUrl}/${row.product_id}">Edit</a> 
+                    <a class="btn btn-secondary"  href="${viewUrl}/${row.product_id}">View</a>
+                    `;
+                },
+                sortable: false,
+            },
+        ],
+    });
+});
